9abdc2481ac2e39058ba48de77a027359fdc985f
kevin thew <kevthew@outlook.com>
2020-06-01 20:27:15 -0600

    fixed an error on import

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {Card, CardTitle} from '../../../node_modules/reactstrap/'
+import {Card, CardTitle, CardText} from '../../../node_modules/reactstrap/'
 
 function Title(props) {
     console.log(props.titleArray.title)
     return(
         <Card>
-            <CardTitle>{props.titleArray.title}</CardTitle>
+            <CardTitle>
+                {props.titleArray.title}
+            </CardTitle>
+            <CardText>{props.titleArray.date}</CardText>
         </Card>
     )
 }
